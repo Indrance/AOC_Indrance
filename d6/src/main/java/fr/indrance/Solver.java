ec17ef44b32d410615e60c19f46e2d213599c045
Indrance <fbampli@gmail.com>
2023-12-06 11:56:09 +0100

    d6 p1 - OOP edition

--- d6/src/main/java/fr/indrance/Solver.java
+++ d6/src/main/java/fr/indrance/Solver.java
@@ -8,35 +8,79 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solver {
-    public class Boat{
+
+    public class Boat {
+
         int charge = -1;
         int speed = 0;
         int distance = 0;
 
-        public Boat(int c){
+        public Boat(int c) {
             charge = c;
         }
 
         // calculate the distance
-        public int runOneStep(){
-            return -99;
+        public void runOneStep() {
+            if (charge > 0){
+                speed++;
+                charge--;
+            } else {
+             distance+= speed;
+            }
         }
+
+        public int getDistance() {
+            return distance;
+        }
+
     }
 
-    public class Race{
+    public class Race {
+
+        int raceID = 0;
         int totalTime = 0;
         int record = 0;
         ArrayList<Boat> allBoat = new ArrayList<>();
         ArrayList<Boat> recordBeater = new ArrayList<>();
 
-        public Race(int t, int r){
+        public Race(int ri, int t, int r) {
+            raceID = ri;
             totalTime = t;
             record = r;
         }
+
+        public void init() {
+            for (int i = 0; i <= totalTime; i++) {
+                allBoat.add(new Boat(i));
+            }
+        }
+
+        public void run() {
+            for (int i = 0; i < totalTime; i++){
+                for(Boat b : allBoat){
+                    b.runOneStep();
+                }
+            }
+            for (Boat b : allBoat){
+                if (b.getDistance() > record){
+                    recordBeater.add(b);
+                }
+            }
+            System.out.println("Race 1 :" + raceID + " Winner : " + recordBeater.size());
+        }
+
+        public int report() {
+            return recordBeater.size();
+        }
+
     }
-    int nbRace = 3;
+
+    int nbRace = 4;
+    int finalResult = 1;
     public void Solve() throws IOException {
-        String file = getClass().getClassLoader().getResource("example.txt").getFile();
+
+
+        String file = getClass().getClassLoader().getResource("d4.txt").getFile();
         System.out.println(file);
         BufferedReader reader = new BufferedReader(new FileReader(file));
         // Get the race time
@@ -46,8 +90,13 @@ public class Solver {
         currentLine = reader.readLine();
         String[] distance = currentLine.split(" ");
         reader.close();
-        for (int i = 0; i < nbRace; i++){
-
+        for (int i = 0; i < nbRace; i++) {
+            Race race = new Race(i, Integer.parseInt(time[i]), Integer.parseInt(distance[i]));
+            race.init();
+            race.run();
+            finalResult*= race.report();
         }
+        System.out.println("Final result = " + finalResult);
     }
+
 }
